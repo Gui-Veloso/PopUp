40ad8ca902b1da052ac4c7003b5dcebc7c7dd23d
Gui-Veloso <111029131+Gui-Veloso@users.noreply.github.com>
2023-12-13 12:10:35 -0300

    Add files via upload

---
+++ Projeto04/Teste-Popup/js/script.js
@@ -0,0 +1,10 @@
+const popup = document.querySelector('.container-popup');
+
+function abrirPopUp(){
+    popup.classList.add('ativo');
+}
+
+
+function fecharPopUp(){
+    popup.classList.remove('ativo');
+}
